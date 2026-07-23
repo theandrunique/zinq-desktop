00906cdfda9a6f37d7e1edc6d1561ff023352a81
theandrunique <theandrunique@gmail.com>
2026-07-23 19:39:26 +0300

    Update TokenStore: make it more robust, log strange situations.
    I'm still not sure this is correct way to use keyring. Probably should
    have read the docs on how to use it, but i'm tired for now.

--- src-tauri/src/auth/token_store.rs
+++ src-tauri/src/auth/token_store.rs
@@ -1,14 +1,15 @@
 use anyhow::{Context};
+use chrono::{DateTime, Utc};
 use tauri::AppHandle;
 use tauri_plugin_keyring::KeyringExt;
 
 use crate::auth::{schemas::TokenPairSchema, types::TokenPair};
 
-const KEYRING_SERVICE: &str = "zinq";
+const KEYRING_SERVICE: &str = "zinq_desktop";
 
 const KEYRING_USER_ACCESS: &str = "access_token";
 const KEYRING_USER_REFRESH: &str = "refresh_token";
-const KEYRING_USER_EXPIRES_IN: &str = "expires_at";
+const KEYRING_USER_EXPIRES_AT: &str = "expires_at";
 
 pub struct TokenStore {
     app_handle: AppHandle,
@@ -32,36 +33,50 @@ impl TokenStore {
 
         self.app_handle
             .keyring()
-            .set_password(KEYRING_SERVICE, KEYRING_USER_EXPIRES_IN, &tokens.expires_at.to_string())
+            .set_password(KEYRING_SERVICE, KEYRING_USER_EXPIRES_AT, &tokens.expires_at.to_string())
             .with_context(|| "Failed to save expires in")?;
 
         Ok(())
     }
 
     pub fn load_tokens(&self) -> Result<Option<TokenPair>, anyhow::Error> {
-        let access_token = self.app_handle
-            .keyring()
-            .get_password(KEYRING_SERVICE, KEYRING_USER_ACCESS)
-            .with_context(|| "Failed to load token")?;
+        let access_token = match self.app_handle.keyring().get_password(KEYRING_SERVICE, KEYRING_USER_ACCESS)? {
+            Some(t) => t,
+            None => return Ok(None),
+        };
 
-        let refresh_token = self.app_handle
-            .keyring()
-            .get_password(KEYRING_SERVICE, KEYRING_USER_REFRESH)
-            .with_context(|| "Failed to load refresh token")?;
+        let refresh_token = match self.app_handle.keyring().get_password(KEYRING_SERVICE, KEYRING_USER_REFRESH)? {
+            Some(t) => t,
+            None => {
+                tracing::warn!("Corrupted keyring: access token exists, but refresh token is missing. Clearing.");
+                self.delete_tokens().ok();
+                return Ok(None);
+            },
+        };
 
-        let expires_at = self.app_handle
-            .keyring()
-            .get_password(KEYRING_SERVICE, KEYRING_USER_EXPIRES_IN)
-            .with_context(|| "Failed to load expires in")?;
+        let expires_at_str = match self.app_handle.keyring().get_password(KEYRING_SERVICE, KEYRING_USER_EXPIRES_AT)? {
+            Some(t) => t,
+            None => {
+                tracing::warn!("Corrupted keyring: expires_at is missing. Clearing.");
+                self.delete_tokens().ok();
+                return Ok(None);
+            },
+        };
 
-        if access_token.is_none() || refresh_token.is_none() || expires_at.is_none() {
-            return Ok(None);
-        }
+        let expires_at = match expires_at_str.parse::<DateTime<Utc>>() {
+            Ok(dt) => dt,
+            Err(error) => {
+                tracing::warn!(%error, raw_value = %expires_at_str, "Error parsing expires at. Clearing corrupted tokens.");
+                self.delete_tokens().ok();
+                return Ok(None);
+            },
+        };
 
+        tracing::trace!("Successfully loaded tokens from keyring");
         Ok(Some(TokenPair {
-            access_token: access_token.unwrap(),
-            refresh_token: refresh_token.unwrap(),
-            expires_at: expires_at.unwrap().parse().expect("Failed to parse expires_at"),
+            access_token: access_token,
+            refresh_token: refresh_token,
+            expires_at: expires_at,
         }))
     }
 
@@ -78,7 +93,7 @@ impl TokenStore {
 
         self.app_handle
             .keyring()
-            .delete_password(KEYRING_SERVICE, KEYRING_USER_EXPIRES_IN)
+            .delete_password(KEYRING_SERVICE, KEYRING_USER_EXPIRES_AT)
             .with_context(|| "Failed to delete expires at")?;
 
         Ok(())
